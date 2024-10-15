61f89592a99951accef00393eac3b05293916faa
praxo019 <84620007+praxo019@users.noreply.github.com>
2024-10-15 00:25:43 -0400

    Add files via upload
    gaip website

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,7 @@
+function Home() {
+    return (
+        <div class="flourish-embed flourish-chart" data-src="visualisation/12243335"><script src="https://public.flourish.studio/resources/embed.js"></script></div>
+    )
+}
+
+export default Home
